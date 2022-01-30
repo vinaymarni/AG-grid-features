2ed099e2c0e0b07de35b58467fc606f132cd369a
vinaymarni <iamvinnu.m@gmail.com>
2022-01-30 18:40:01 +0530

    dynamic styling

--- my-app/src/Table.js
+++ my-app/src/Table.js
@@ -2,31 +2,30 @@ import React from 'react'
 import {AgGridReact} from 'ag-grid-react'
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import "./Table.css"
 
 export const Table = () => {
-    const actionButton = (params) =>{
-        console.log(params)
-    }
+    
+    
     const data = [
         {name : "Vinay", age:24 , salery: 300000 },
-        {name : "Bhavani", age:23, salery: 350000 },
+        {name : "Bhavani", age:18, salery: 350000 },
         {name : "Achyuth", age:24, salery: 360000 },
         {name : "Sanjey", age:25, salery: 370000 },
         {name : "Sai", age:26, salery: 380000 }
     ]
     const columns =[
         {headerName:'Name', field:"name"},
-        {headerName:'Age', field:"age"},
+        {headerName:'Age', field:"age",
+        //cellStyle:(params) => (params.value > 20 ? {borderLeft: "4px green solid" } : {background: "red", color:"white" })},
+        cellClass:(params) => (params.value > 20 ? "moreThen20" : "lessThen20")},
         {headerName:'Salery', field:"salery"},
-        {headerName:'Action',
-        cellRendererFramework:(params) => 
-        <div>
-            <button onClick={actionButton(params)}>Click me</button>
-        </div> },
+       
+        
     ]
 
     const defaultColDef={
-        sortable: true, editable: true, filter: true, floatingFilter: true, flex:1
+        sortable: true, editable: true, filter: true
     }
 
     
