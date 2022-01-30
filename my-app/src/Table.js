ddd60b29e84edb368aca6dbc5791c3b216a46abd
vinaymarni <iamvinnu.m@gmail.com>
2022-01-30 19:29:15 +0530

    fetch-data

--- my-app/src/Table.js
+++ my-app/src/Table.js
@@ -5,17 +5,11 @@ import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 export const Table = () => {
     
-    const data = [
-        {name : "Vinay", age:24 , salery: 300000 },
-        {name : "Bhavani", age:23, salery: 350000 },
-        {name : "Achyuth", age:24, salery: 360000 },
-        {name : "Sanjey", age:25, salery: 370000 },
-        {name : "Sai", age:26, salery: 380000 }
-    ]
+   
     const columns =[
+        {headerName:'ID', field:"id"},
         {headerName:'Name', field:"name"},
-        {headerName:'Age', field:"age", tooltipField:"name"},
-        {headerName:'Salery', field:"salery"},
+        {headerName:'Email', field:"email"},
         
     ]
 
@@ -23,21 +17,17 @@ export const Table = () => {
         sortable: true, editable: true, filter: true, floatingFilter: true, flex:1, tooltipField:"name"
     }
 
-    let gridApi = "";
     const onGridReady = (params) =>{
-        gridApi = params.api
-    }
-
-    const onExportClick = () =>{
-        gridApi.exportDataAsCsv()
+        fetch("https://jsonplaceholder.typicode.com/comments").then((resp) => resp.json())
+        .then((resp)=> {console.log(resp)
+            params.api.applyTransaction({add:resp})})
     }
 
     return(
         <div>
-            <button onClick={() => onExportClick()}>export</button>
             <div id="myGrid" className='ag-theme-alpine' style={{height:"250px", width:"600px"}} >
                 <AgGridReact 
-                rowData = {data} 
+               
                 columnDefs={columns} 
                 defaultColDef={defaultColDef} 
                 onGridReady={onGridReady} 
