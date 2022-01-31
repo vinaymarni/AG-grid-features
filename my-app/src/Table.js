539d26564b4aeb448706b7982ce2581fd66122a6
vinaymarni <iamvinnu.m@gmail.com>
2022-01-31 16:00:06 +0530

    pagination

--- my-app/src/Table.js
+++ my-app/src/Table.js
@@ -4,36 +4,64 @@ import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 export const Table = () => {
-    
-   
     const columns =[
-        {headerName:'ID', field:"id"},
+        {headerName:'ID', field:"id", checkboxSelection: true, headerCheckboxSelection: true },
         {headerName:'Name', field:"name"},
         {headerName:'Email', field:"email"},
-        
+        {headerName:'Body', field:"body"},
     ]
 
     const defaultColDef={
-        sortable: true, editable: true, filter: true, floatingFilter: true, flex:1, tooltipField:"name"
+        sortable: true, editable: true, filter: true, floatingFilter: true, flex : 1, tooltipField:"name"
     }
-
+    let gridApi = ""
     const onGridReady = (params) =>{
+        gridApi = params
         fetch("https://jsonplaceholder.typicode.com/comments").then((resp) => resp.json())
         .then((resp)=> {console.log(resp)
-            params.api.applyTransaction({add:resp})})
+            params.api.applyTransaction({add:resp})
+            //params.api.paginationGoToPage(10)
+        })
     }
 
+    const onPaginationChange = (pageSize) => {
+        gridApi.api.paginationGoToPage(pageSize)
+    }
+
+    const rowSelectionType = "multiple" 
+
+    const onSelectionChanged = (event) =>{
+        console.log(event.api.getSelectedRows())
+    }
+
+    
+
     return(
-        <div>
-            <div id="myGrid" className='ag-theme-alpine' style={{height:"250px", width:"600px"}} >
+    <div style={{margin: "20px"}}>
+            <h3>Student Details</h3>
+            <select onChange={(e) => onPaginationChange(e.target.value) }>
+                <option value = "0">0</option>
+                <option value = "10">10</option>
+                <option value = "20">20</option>
+                <option value = "30">30</option>
+                <option value = "40">40</option>
+                <option value = "50">50</option>
+            </select>
+            <div id="myGrid" className='ag-theme-alpine' style={{height:"350px"}} >
                 <AgGridReact 
-               
                 columnDefs={columns} 
                 defaultColDef={defaultColDef} 
                 onGridReady={onGridReady} 
                 enableBrowserTooltips={true}
                 tooltipShowDelay = {{tooltipShowDelay:2}}
-                />
+                rowSelection={rowSelectionType}
+                onSelectionChanged = {onSelectionChanged}
+                rowMultiSelectWithClick ={true}
+                pagination = {true}
+                paginationPageSize = {10}
+                //paginationAutoPageSize = {true}
+                >
+                </AgGridReact>
             </div>
         </div>
     )
