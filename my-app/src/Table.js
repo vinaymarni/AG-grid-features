686fd65f598439f3da6294454cefe37580474b76
vinaymarni <iamvinnu.m@gmail.com>
2022-02-01 17:27:23 +0530

    transection

--- my-app/src/Table.js
+++ my-app/src/Table.js
@@ -1,72 +1,159 @@
 import React, { useState } from 'react';
-import { AgGridReact } from 'ag-grid-react'
+import { AgGridReact, AgGridColumn } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
-//import 'ag-grid-enterprise';
-import axios from 'axios';
 
 const Table = () => {
-    const [data, setdata] = useState(null);
-    const [gridColumnApi , setGridColumnApi] = useState(null)
-    const [hideColumn , setHideColumn] = useState(false)
-    const onGridReady = (params) => {
-        setdata(params.api)
-        axios.get('https://www.ag-grid.com/example-assets/row-data.json')
-        .then(res => res.data).then(res => {
-            params.api.applyTransaction({add:res})
-        })
-        setGridColumnApi(params.columnApi)
-    };
+  const [gridApi, setGridApi] = useState(null);
+  const [gridColumnApi, setGridColumnApi] = useState(null);
 
-    const columns = [
-        {headerName : "Make" , field : 'make' , checkboxSelection : true , headerCheckboxSelection : true} ,
-        {headerName : "Model" , field : 'model' , tooltipField : 'make'} ,
-        {headerName : "Price" , field : 'price' , cellStyle:((params) => (params.value > 50000 ? {color : 'green'} : {color : 'red'}))}
-    ]
-    const defaultColDefs = {sortable : true , editable : true , filter : true , floatingFilter : true , flex: 1 }
-   
-    const onSelectionChanged = (event) => {
-        console.log(event.api.getSelectedRows())
-    }
-    const onPageChange = (pageNum) => {
-        data.paginationSetPageSize(pageNum)
-    }
+  const onGridReady = (params) => {
+    setGridApi(params.api);
+    setGridColumnApi(params.columnApi);
+  };
 
-    let buttonText = "Hide"
-    hideColumn ? buttonText = "Show" : buttonText = "Hide"
-    
-    const showColumn = () => {
-        gridColumnApi.setColumnsVisible(['model'] , hideColumn)
-        setHideColumn(!hideColumn)
-        data.sizeColumnsToFit()
-        
-    }
-    const onTextChange = (e) => {
-        data.setQuickFilter(e.target.value)
-    } 
-    return(
-      <div ag-grid="gridOptions" className='ag-theme-alpine' style={{height : '95vh' , width : '95vw', marginLeft: "10px"}}>
-        <div style={{ backgroundColor: '#919291', padding : "10px"}}>
-         <select onChange={(e) => onPageChange(e.target.value)} style={{width : '100px' , height : '25px', borderRadius: "15PX", paddingLeft: "7px", paddingRight: "7px"}}>
-              <option value='25'>25</option>
-              <option value='50'>50</option>
-              <option value='75'>75</option>
-              <option value='100'>100</option>
-          </select>
-          <button style = {{width : '100px' , height : '25px' , marginLeft : '10px' , backgroundColor : 'white' , borderWidth : '1px', borderRadius: "15PX"}} onClick={showColumn}>{buttonText}</button>
-          <input type = 'search' onChange={(e) => onTextChange(e)} style = {{width : '350px' , height : '25px' , marginLeft : '10px' , borderWidth : '1px', borderRadius: "15PX", padding: "7px"}} placeholder='Search Here..' />
+  const getRowData = () => {
+    const rowData = [];
+    gridApi.forEachNode(function (node) {
+      rowData.push(node.data);
+    });
+    console.log('Row Data:');
+    console.log(rowData);
+  };
+
+  const clearData = () => {
+    gridApi.setRowData([]);
+  };
+
+  const addItems = (addIndex) => {
+    const newItems = [
+      createNewRowData(),
+      createNewRowData(),
+      createNewRowData(),
+    ];
+    const res = gridApi.applyTransaction({
+      add: newItems,
+      addIndex: addIndex,
+    });
+    printResult(res);
+  };
+
+  const updateItems = () => {
+    const itemsToUpdate = [];
+    gridApi.forEachNodeAfterFilterAndSort(function (rowNode, index) {
+      if (index >= 2) {
+        return;
+      }
+      const data = rowNode.data;
+      data.price = Math.floor(Math.random() * 20000 + 20000);
+      itemsToUpdate.push(data);
+    });
+    const res = gridApi.applyTransaction({ update: itemsToUpdate });
+    printResult(res);
+  };
+
+  const onRemoveSelected = () => {
+    const selectedData = gridApi.getSelectedRows();
+    const res = gridApi.applyTransaction({ remove: selectedData });
+    printResult(res);
+  };
+
+  return (
+    <div style={{ width: '100%', height: '100%' }}>
+      <div style={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+        <div style={{ marginBottom: '4px' }}>
+          <button onClick={() => addItems()}>Add Items</button>
+          <button onClick={() => addItems(2)}>Add Items addIndex=2</button>
+          <button onClick={() => updateItems()}>Update Top 2</button>
+          <button onClick={() => onRemoveSelected()}>Remove Selected</button>
+          <button onClick={() => getRowData()}>Get Row Data</button>
+          <button onClick={() => clearData()}>Clear Data</button>
         </div>
-        <AgGridReact
-          columnDefs = {columns}
-          defaultColDef = {defaultColDefs}
-          pagination = {true}
-          enableBrowserTooltips = {true}
-          onGridReady = {onGridReady}
-          rowSelection = 'multiple'
-          onSelectionChanged = {onSelectionChanged}
-          />
+        <div style={{ flexGrow: '1' }}>
+          <div
+            id="myGrid"
+            style={{
+              height: '100%',
+              width: '100%',
+            }}
+            className="ag-theme-alpine"
+          >
+            <AgGridReact
+              rowData={[
+                {
+                  make: 'Toyota',
+                  model: 'Celica',
+                  price: 35000,
+                  zombies: 'Elly',
+                  style: 'Smooth',
+                  clothes: 'Jeans',
+                },
+                {
+                  make: 'Ford',
+                  model: 'Mondeo',
+                  price: 32000,
+                  zombies: 'Shane',
+                  style: 'Filthy',
+                  clothes: 'Shorts',
+                },
+                {
+                  make: 'Porsche',
+                  model: 'Boxter',
+                  price: 72000,
+                  zombies: 'Jack',
+                  style: 'Dirty',
+                  clothes: 'Padded',
+                },
+              ]}
+              defaultColDef={{ flex: 1 }}
+              rowSelection={'multiple'}
+              animateRows={true}
+              onGridReady={onGridReady}
+            >
+              <AgGridColumn field="make" />
+              <AgGridColumn field="model" />
+              <AgGridColumn field="price" />
+              <AgGridColumn field="zombies" />
+              <AgGridColumn field="style" />
+              <AgGridColumn field="clothes" />
+            </AgGridReact>
+          </div>
+        </div>
       </div>
-  )
+    </div>
+  );
+};
+
+let newCount = 1;
+function createNewRowData() {
+  const newData = {
+    make: 'Toyota ' + newCount,
+    model: 'Celica ' + newCount,
+    price: 35000 + newCount * 17,
+    zombies: 'Headless',
+    style: 'Little',
+    clothes: 'Airbag',
+  };
+  newCount++;
+  return newData;
+}
+function printResult(res) {
+  console.log('---------------------------------------');
+  if (res.add) {
+    res.add.forEach(function (rowNode) {
+      console.log('Added Row Node', rowNode);
+    });
+  }
+  if (res.remove) {
+    res.remove.forEach(function (rowNode) {
+      console.log('Removed Row Node', rowNode);
+    });
+  }
+  if (res.update) {
+    res.update.forEach(function (rowNode) {
+      console.log('Updated Row Node', rowNode);
+    });
+  }
 }
 
-export default Table
+export default Table;
